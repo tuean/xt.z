a9135a6d1584fbdc4417dfd0c6d2b1648ceb699a
tuean <tuean_z@163.com>
2018-03-07 21:13:46 +0800

    sikuliX使用调研

--- src/main/java/selenium/demo.java
+++ src/main/java/selenium/demo.java
@@ -1,5 +1,6 @@
 package selenium;
 
+import com.sikulix.api.Window;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,46 +9,51 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
+import java.util.concurrent.TimeUnit;
+import org.sikuli.script.Screen;
+
 /**
  * Created by zhongxiaotian on 2018/3/4.
  */
 public class demo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
-//        System.setProperty("webdriver.safari.driver", "/Users/zhongxiaotian/Library/Safari/Extensions");
-//        System.setProperty("webdriver.safari.noinstall", "true");
+        //车牌地址
+//        String url = "http://paimai.alltobid.com";
+        String url = "http://www.baidu.com";
+
 
         WebDriver dr =  new SafariDriver();
+        dr.manage().window().maximize();
+        dr.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        dr.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        dr.get(url);
+//        WebElement element = dr.findElement(By.id("kw"));
+//        element.sendKeys("selenuim");
 
-        dr.get("http://www.baidu.com");
+//        WebElement searchClick = dr.findElement(By.id("su"));
+//        searchClick.click();
 
-        WebElement element = dr.findElement(By.id("kw"));
-        element.sendKeys("selenuim");
-
-        WebElement searchClick = dr.findElement(By.id("su"));
-        searchClick.click();
-
-
-//        dr.findElement(By.id("kw")).sendKeys("hello Selenium");
-//
-//        dr.findElement(By.id("su")).click();
-
-//        try {
-//
-//            Thread.sleep(3000);
-//
-//        } catch (InterruptedException e) {
-//
-//            // TODO Auto-generated catch block
-//
-//            e.printStackTrace();
-//
-//        }
+        Thread.sleep(1000);
+        WebElement logo = dr.findElement(By.xpath("//img[@src='//www.baidu.com/img/bd_logo1.png']"));
+        Thread.sleep(1000);
+        logo.click();
+        System.out.println(logo.getLocation().x);
 
 
+        //sikuliX
+        String path = "/Users/zhongxiaotian/Desktop/pic";
+        Screen screen = new Screen();
 
-//        dr.quit();
+        try{
 
+        }catch (Exception var){
+            var.printStackTrace();
+        }
+//        Window.openURL(url);
+
+        Thread.sleep(100000L);
+        dr.quit();
     }
 }
